1802162711b7116c85003da1b22c4647cc1443a0
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:54:38 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8DersDictionary // Dictionariler sorted list gibidir. Fark olarak, elemanlar oldugu gibi listeye eklenirler, sıralanmazlar...
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> sozluk = new Dictionary<string, int>();
+            sozluk.Add("Zehra", 15);
+            sozluk.Add("Ahmet", 25);
+            sozluk.Add("Hasan", 35);
+            sozluk.Add("Fatih", 45);
+
+            foreach (var item in sozluk)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
